9110ba6262909b77ba3a0cfbfa4fca68a615780b
Moreshwar <moreshwars20@gmail.com>
2026-07-11 21:45:29 +0530

    Trade app

--- trade_predictor/FORECAST_ONLY.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title NSE Forecast
-cd /d "%~dp0"
-where py >nul 2>&1 && (set PY=py) || (set PY=python)
-echo Next-day forecast (using the last trained model)...
-echo.
-%PY% predict_next.py
-echo.
-pause
--- trade_predictor/SHOW_ACCURACY.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title Model Accuracy
-cd /d "%~dp0"
-where py >nul 2>&1 && (set PY=py) || (set PY=python)
-%PY% -u accuracy.py
-echo.
-pause
